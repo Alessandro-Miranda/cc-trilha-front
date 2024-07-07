71325379a45a1fa29447c7520205b88af012aba0
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2024-07-07 12:40:41 -0300

    feat: create header section in mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+import { motion } from 'framer-motion';
+import { useRef } from 'react';
+import { AnimatedPageTransition } from './components/ui/AnimatedPageTransition';
+import { DecorationImage } from './components/ui/DecorationImage';
+import { Divider } from './components/ui/Divider';
+import { ScrollDownButton } from './components/ui/ScrollDownButton';
+import { SocialMedias } from './components/ui/SocialMedias';
+
 function App() {
+
+  const sectionTargetToScrollDown = useRef<HTMLElement|null>(null);
+
   return (
     <>
-      <p className="font-extrabold">Hello world!</p>
+      <motion.main
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, ease: [0.22, 1, 0.36, 1] }}
+        className="px-5 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl overflow-x-hidden"
+      >
+        <header className="relative py-36">
+          <h1 className="text-4xl font-semibold ">Alessandro Miranda</h1>
+          <h2 className="font-subHeading text-lg mb-5">Desenvolvedor Front-end</h2>
+          <p className="text-sm">
+            Desenvolvedor em busca de criar sites leves, minimalistas, mas que atendam às
+            necessidades dos usuários de forma eficaz.
+          </p>
+
+          <SocialMedias />
+
+          <DecorationImage />
+          <ScrollDownButton target={sectionTargetToScrollDown} />
+        </header>
+
+        <Divider />
+
+        <section className="py-36" ref={sectionTargetToScrollDown}>
+          
+        </section>
+      </motion.main>
+
+      <AnimatedPageTransition />
     </>
   );
 }
