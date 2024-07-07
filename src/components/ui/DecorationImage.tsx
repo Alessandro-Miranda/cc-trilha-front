c88c518fbf92071d8b2e119db11fac44b70bebbd
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2024-07-07 12:38:51 -0300

    feat: create component with blured image used as a decoration in header or another sections

---
+++ src/components/ui/DecorationImage.tsx
@@ -0,0 +1,10 @@
+export function DecorationImage() {
+  return (
+    <img
+      src="/decoration.svg"
+      alt="Imagem de decoração"
+      aria-hidden
+      className="absolute top-0 -right-36 -z-10"
+    />
+  );
+}
