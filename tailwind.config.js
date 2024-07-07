40268cb5a3d7b163bc4bf174201652313fc7f8f2
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2024-07-07 00:27:44 -0300

    style: insert font-family and colors to default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#000000',
+        red: '#e53939',
+        white: '#ffffff',
+      },
+      fontFamily: {
+        headings: ['Montserrat', 'sans-serif'],
+        subHeading: ['Lora', 'sans-serif'],
+        text: ['Hind Madurai', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
