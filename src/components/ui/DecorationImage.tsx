7888e3976e76ed6840e41e04d16810e1d5f4d87b
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2024-07-07 21:41:00 -0300

    feat: add breakpoints to large and extreme large devices

--- src/components/ui/DecorationImage.tsx
+++ src/components/ui/DecorationImage.tsx
@@ -4,7 +4,7 @@ export function DecorationImage() {
       src="/decoration.svg"
       alt="Imagem de decoração"
       aria-hidden
-      className="absolute top-0 -right-36 -z-10"
+      className="absolute top-0 -right-36 -z-10 sm:-right-72 sm:-top-40 md:-top-96 md:-right-80 lg:-top-20"
     />
   );
 }
--- src/components/ui/ScrollDownButton.tsx
+++ src/components/ui/ScrollDownButton.tsx
@@ -14,12 +14,10 @@ export function ScrollDownButton(
 
   return (
     <button
-      className="uppercase absolute -right-3 bottom-28 rotate-90"
+      className="uppercase absolute -right-3 bottom-28 rotate-90 lg:-left-10 lg:right-auto lg:bottom-20 2xl:bottom-36"
       onClick={() => onScrollClick(target)}
     >
-      <span className="inline-block -rotate-180 text-[10px]">
-        Scroll down
-      </span>
+      <span className="inline-block -rotate-180 text-[10px]">Scroll down</span>
 
       <motion.img
         src="/scroll-down.svg"
