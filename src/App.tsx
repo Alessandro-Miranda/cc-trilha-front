ed4ca8247426873c201f518d852abba9637ed987
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2024-07-08 11:31:49 -0300

    fix: update title from second hobbie card

--- src/App.tsx
+++ src/App.tsx
@@ -95,8 +95,8 @@ function App() {
             />
 
             <HobbiesCard
-              title="Tocar Violão"
-              image={{ src: PlayGuitar, alt: 'Hobbie - Tocar Violão' }}
+              title="Tocar e estudar música"
+              image={{ src: PlayGuitar, alt: 'Hobbie - Tocar e estudar música' }}
             />
 
             <HobbiesCard
